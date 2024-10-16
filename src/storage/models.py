ae369c11f2906d651b866cdd64afb91cef3bd6ef
Anikavuk <usla_2018@mail.ru>
2024-10-16 16:24:42 +0500

    fix: fixed a bug with relationship(lazy='joined')

--- src/storage/models.py
+++ src/storage/models.py
@@ -25,15 +25,14 @@ class MatchesOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     uuid: Mapped[str] = mapped_column(CHAR(36), primary_key=True, nullable=False, unique=True,
                                       default=lambda: str(uuid.uuid4()))
-    # mapped_column(unique=True, nullable=False)
     player1: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
     player2: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
     winner: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
     score: Mapped[JSON] = mapped_column(JSON)
 
-    player1_relation = relationship("PlayersOrm", foreign_keys=[player1])
-    player2_relation = relationship("PlayersOrm", foreign_keys=[player2])
-    winner_relation = relationship("PlayersOrm", foreign_keys=[winner])
+    player1_relation = relationship("PlayersOrm", foreign_keys=[player1], lazy='joined')
+    player2_relation = relationship("PlayersOrm", foreign_keys=[player2], lazy='joined')
+    winner_relation = relationship("PlayersOrm", foreign_keys=[winner], lazy='joined')
 
     def __repr__(self):
         return (f"MatchesOrm(id={self.id}, uuid={self.uuid}, "
