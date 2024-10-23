a60c259f3b1227326b7b78b0c67b10e0df0da3a3
Anikavuk <usla_2018@mail.ru>
2024-10-23 21:32:06 +0500

    fix: MatchManager

--- src/storage/config.py
+++ src/storage/config.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import joinedload, aliased
+
 from database import engine, Session
 from errors import ErrorResponse, IntegrityError, DatabaseErrorException
 from models import PlayersOrm, MatchesOrm
@@ -29,40 +31,84 @@ class PlayerManager:
             # то исключение OperationalError выбрала или не то
             return ErrorResponse.error_response(exception=DatabaseErrorException())
 
-    class MatchManager:
-        def __init__(self, player1, player2, winner, score):
-            self.player1 = player1
-            self.player1 = player2
-            self.winner = winner
-            self.score = score
+class MatchManager:
+    def __init__(self, player1, player2, winner, score):
+        self.player1 = player1
+        self.player2 = player2
+        self.winner = winner
+        self.score = score
 
-        def save_matches(self):
-            try:
-                with Session(autoflush=False, bind=engine) as db:
-                    match = MatchesOrm()
-                    db.add(match)
-                    db.commit()
-            except IntegrityError:
-                return ErrorResponse.error_response(exception=IntegrityError())
 
-        def get_all_matches(self):
-            try:
-                with Session(autoflush=False, bind=engine) as db:
-                    matches = db.query(MatchesOrm).all()
-                    all_matches = [(match.id,
-                                    match.player1,
-                                    match.player2,
-                                    match.winner,
-                                    match.score)
-                                   for match in matches]
-                    return all_matches
+    def save_matches(self):
+        try:
+            with Session(autoflush=False, bind=engine) as db:
+                match = MatchesOrm(player1=self.player1,
+                                   player2=self.player2,
+                                   winner=self.winner,
+                                   score=self.score)
+                db.add(match)
+                db.commit()
+        except IntegrityError:
+            return ErrorResponse.error_response(exception=IntegrityError())
 
-            except OperationalError:
-                # Алсу!Проверить ошибку недоступности базы данных в sqlalchemy!!!
-                # то исключение OperationalError выбрала или не то
-                return ErrorResponse.error_response(exception=DatabaseErrorException())
-    def is_player_name_in_database(self, player_name):
-        """Проверка имени в базе данных, нужна эта функция или нет"""
-        with Session(autoflush=False, bind=engine) as db:
-            first = db.query(PlayersOrm).filter(PlayersOrm.name == player_name)
-            pass
+    def get_all_matches(self):
+        # try:
+
+        with (Session(autoflush=False, bind=engine) as bd):
+            matches_query = bd.query(MatchesOrm.id,
+                                     MatchesOrm.score,
+                                     PlayersOrm.name.label('player1'),
+                                     PlayersOrm.name.label('player2'),
+                                     PlayersOrm.name.label('winner')
+                                     ).join(PlayersOrm, MatchesOrm.player1 == PlayersOrm.id
+                                            ).join(PlayersOrm, MatchesOrm.player2 == PlayersOrm.id
+                                                   ).join(PlayersOrm, MatchesOrm.winner == PlayersOrm.id)
+            results = matches_query.all()
+
+            # Форматируем результаты для удобства
+            matches = []
+            for match in results:
+                matches.append({
+                    'match_id': match.id,
+                    'player1': match.player1_relation.name,
+                    'player2': match.player2_relation.name,
+                    'winner': match.winner_relation.name,
+                    'score': match.score
+                })
+
+            return results
+        # except OperationalError:
+        #     # Алсу!Проверить ошибку недоступности базы данных в sqlalchemy!!!
+        #     # то исключение OperationalError выбрала или не то
+        #     return ErrorResponse.error_response(exception=DatabaseErrorException())
+
+    def list_player_matches(self, player_name):
+        try:
+            with Session(autoflush=False, bind=engine) as db:
+                matches = db.query(MatchesOrm).filter(self.name==player_name).all()
+                all_matches = [(match.id,
+                                match.player1,
+                                match.player2,
+                                match.winner,
+                                match.score)
+                               for match in matches]
+                return all_matches
+        except OperationalError:
+            # Алсу!Проверить ошибку недоступности базы данных в sqlalchemy!!!
+            # то исключение OperationalError выбрала или не то
+            return ErrorResponse.error_response(exception=DatabaseErrorException())
+
+
+# def is_player_name_in_database(self, player_name):
+#     """Проверка имени в базе данных, нужна эта функция или нет надо решить"""
+#     with Session(autoflush=False, bind=engine) as db:
+#         first = db.query(PlayersOrm).filter(PlayersOrm.name == player_name)
+#         pass
+
+
+ff = PlayerManager('Дэн') # [(5, 'Даша'), (1, 'Денис'), (7, 'Дима'), (8, 'Дэн'), (2, 'Женя'), (4, 'Максим'), (3, 'Оля'), (6, 'Юра')]
+# print(ff.save_player())
+# print(ff.get_all_players())
+mm = MatchManager(7, 1, 7, 3)
+# mm.save_matches()
+print(mm.get_all_matches())
--- src/storage/models.py
+++ src/storage/models.py
@@ -10,7 +10,7 @@ class Base(DeclarativeBase):
 
 
 class PlayersOrm(Base):
-    __tablename__: str = "players"
+    __tablename__: str = 'players'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(VARCHAR(length=255), unique=True, nullable=False)
@@ -20,21 +20,21 @@ class PlayersOrm(Base):
 
 
 class MatchesOrm(Base):
-    __tablename__: str = "matches"
+    __tablename__: str = 'matches'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     uuid: Mapped[str] = mapped_column(CHAR(36), primary_key=True, nullable=False, unique=True,
                                       default=lambda: str(uuid.uuid4()))
-    player1: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
-    player2: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
-    winner: Mapped[int] = mapped_column(ForeignKey("players.id", ondelete="CASCADE"))
+    player1: Mapped[int] = mapped_column(ForeignKey('players.id', ondelete='CASCADE'))
+    player2: Mapped[int] = mapped_column(ForeignKey('players.id', ondelete='CASCADE'))
+    winner: Mapped[int] = mapped_column(ForeignKey('players.id', ondelete='CASCADE'))
     score: Mapped[JSON] = mapped_column(JSON)
 
-    player1_relation = relationship("PlayersOrm", foreign_keys=[player1], lazy='joined')
-    player2_relation = relationship("PlayersOrm", foreign_keys=[player2], lazy='joined')
-    winner_relation = relationship("PlayersOrm", foreign_keys=[winner], lazy='joined')
+    player1_relation = relationship('PlayersOrm', foreign_keys=[player1], lazy='joined')
+    player2_relation = relationship('PlayersOrm', foreign_keys=[player2], lazy='joined')
+    winner_relation = relationship('PlayersOrm', foreign_keys=[winner], lazy='joined')
 
     def __repr__(self):
-        return (f"MatchesOrm(id={self.id}, uuid={self.uuid}, "
+        return (f"MatchesOrm(id={self.id}, uuid={self.uuid},"
                 f"player1={self.player1}, player2={self.player2},"
                 f"winner={self.winner},score={self.score}")
