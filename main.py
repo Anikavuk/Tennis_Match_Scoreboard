a5b7f4d329978812f728bb1a068f1d2e080da87a
Anikavuk <usla_2018@mail.ru>
2024-11-09 21:39:26 +0500

    fix: main (WhiteNoise())

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ logger.setLevel(logging.ERROR)
 
 
 if __name__ == '__main__':
-    app = application
-    app = WhiteNoise(app, root='src/view/static/')
-    serve(TransLogger(application, setup_console_handler=False))
-    serve(application, host='0.0.0.0', port=8080)
+    app = WhiteNoise(application)
+    app.add_files('src/view/static/', prefix='static/')
+    serve(TransLogger(app, setup_console_handler=False))
+    serve(app, host='0.0.0.0', port=8080)
 
